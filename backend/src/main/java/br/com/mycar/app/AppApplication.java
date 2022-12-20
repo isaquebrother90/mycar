a441b1975860d7e2d1e3d46324fa0c9a0e81fdc4
isaquebrother90 <improgramcode@gmail.com>
2022-12-20 12:11:35 -0300

    Project created

---
+++ backend/src/main/java/br/com/mycar/app/AppApplication.java
@@ -0,0 +1,13 @@
+package br.com.mycar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
